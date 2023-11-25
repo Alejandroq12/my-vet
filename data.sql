cb432b041cf1f4d29bba004e82fe8934308dc90d
Alejandroq12 <julio.quezada@catolica.edu.sv>
2023-11-25 13:03:12 -0600

    Feat: Add queries to update animals table.

--- data.sql
+++ data.sql
@@ -27,3 +27,6 @@ INSERT INTO species (name)
 VALUES
 ('Pokemon'),
 ('Digimon');
+
+UPDATE animals SET species_id = ( SELECT id FROM species WHERE name = 'Digimon') WHERE name LIKE '%mon';
+UPDATE animals SET species_id = ( SELECT id FROM species WHERE name = 'Pokemon') WHERE name NOT LIKE '%mon';
