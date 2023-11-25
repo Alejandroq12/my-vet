360d7221d8913eb973b5622d8fbd6362b6eea632
Alejandroq12 <julio.quezada@catolica.edu.sv>
2023-11-25 15:24:48 -0600

    Feat: Add query to create vets table.

--- schema.sql
+++ schema.sql
@@ -29,3 +29,6 @@ ALTER TABLE animals ADD FOREIGN KEY(species_id) REFERENCES species(id);
 ALTER TABLE animals ADD COLUMN owner_id INT;
 ALTER TABLE animals ADD FOREIGN KEY(owner_id) REFERENCES owners(id);
 COMMIT;
+
+CREATE TABLE vets (id SERIAL PRIMARY KEY, name VARCHAR(50) NOT NULL, age INT NOT NULL, date_of_graduation DATE NOT NULL);
+
